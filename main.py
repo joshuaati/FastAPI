570b60dead545772639a206c0e95d2f7ca1af641
joshuaati <joshuaati97@gmail.com>
2022-12-22 22:58:31 +0100

    changes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
+from pydentic import BaseModel
 
 app = FastAPI()
 
 users = []
+
+class User(BaseModel):
+    email: str
+    is_active:bool
 
 @app.get("/users")
 async def get_users():
     return users
 
 @app.post("/users")
-async def create_user(user):
+async def create_user(user: User):
     users.append(user)
-    return {'Message'}
+    return {'Success'}
