370d945adc7296ee1b262d5a5aa6277d4c4944a8
joshuaati <joshuaati97@gmail.com>
2022-12-22 22:47:34 +0100

    changes

---
+++ README.md
@@ -0,0 +1,1 @@
+I followed the tutorials on FastAPI by Faraday Academy :: https://www.youtube.com/watch?v=gQTRsZpR7Gw&t=956s
---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = []
+
+@app.get("/users")
+async def get_users():
+    return users
+
+@app.post("/users")
+async def create_user(user):
+    users.append(user)
+    return {'Message'}
