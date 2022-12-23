587b319d59e635a0db74595225a6f899e1358a73
joshuaati <joshuaati97@gmail.com>
2022-12-23 22:23:10 +0100

    23/12/2020

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI
-from pydentic import BaseModel
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+    title = "Fast API LMS"
+    description= "LMS for managing students and courses"
+    version="0.0.1"
+    contact={
+        "name":"T", 
+        "email":"t@example.com"},
+    
+)
 
 users = []
 
 class User(BaseModel):
     email: str
     is_active:bool
+    bio: Optional[str] 
 
-@app.get("/users")
+@app.get("/users", response_model=List[User])
 async def get_users():
     return users
 
@@ -17,3 +27,10 @@ async def get_users():
 async def create_user(user: User):
     users.append(user)
     return {'Success'}
+
+
+@app.get("/users/{id}")
+async def get_user(
+    id: int = Path(..., description="The ID of the user you want to retrieve"), 
+    q: str = Query(None, max_length=5)):
+    return {"user":users[id], "query": q}
